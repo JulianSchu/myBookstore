bf88cd8a7a6f2d0077ce803cdc8ad7821b6447a2
JulianSchu <jun.xu@hotmail.dem>
2019-05-03 15:46:00 +0200

    bookstore vue

--- main.js
+++ main.js
@@ -10,21 +10,17 @@ let vm = new Vue({
             fetch('https://api.myjson.com/bins/udbm5')
                 .then(res => res.json())
                 .then(data => {
-                let books = data.books;
-                books.forEach(function(each){
-                    if (books.indexOf(each) === 0) {
-                        each.carousel_class = 'carousel-item active'
-                    } else {
-                        each.carousel_class = 'carousel-item'
-                    }
-                })
-                    this.books = books;
-                    console.log(this.books)
-
+                    this.books = data.books;
                 })
                 .catch(err => console.log(err))
         },
-        nullResult(){
+        searchedBooks() {
+            let str = this.search;
+            return this.books.filter(function (book) {
+                if (book.titulo.match(str) || book.descripcion.match(str) || book.idioma.match(str)) return true;
+            })
+        },
+        nullResult() {
             let result = this.filteredBooks;
             if (result.length === 0) {
                 this.footer = true;
@@ -34,11 +30,16 @@ let vm = new Vue({
         }
     },
     computed: {
-        filteredBooks: function () {
-            let str = this.search;
-            return this.books.filter(function (book) {
-                if (book.titulo.match(str) || book.descripcion.match(str) || book.idioma.match(str)) return true;
-            })
+        filteredBooks() {
+            let results = this.searchedBooks();
+            results.forEach(function (each) {
+                if (results.indexOf(each) === 0) {
+                    each.carousel_class = 'carousel-item active'
+                } else {
+                    each.carousel_class = 'carousel-item'
+                }
+            });
+            return results
         }
     },
     mounted() {
